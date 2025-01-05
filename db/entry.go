9ab476c7745ef44219248ca47fb4dd2130fbee2e
Gun on Ume-Pro <napat.gunt3001@gmail.com>
2025-01-04 17:24:07 -0800

    Proper display of entry date & keyword in list title

--- db/entry.go
+++ db/entry.go
@@ -1,14 +1,39 @@
 // Interface implementation for Entries type
 package db
 
+import (
+	"fmt"
+	"time"
+)
+
 func (e Entry) FilterValue() string {
 	return e.Content
 }
 
 func (e Entry) Title() string {
-	return e.Date
+	date, err := e.getEntryDateAsTime()
+	if err != nil {
+		return "Invalid entry date"
+	}
+	dateString := date.Format("2 Jan '06")
+	return fmt.Sprintf("%s (%s) - %s", dateString, e.getRelativeDateString(), e.Keyword)
 }
 
 func (e Entry) Description() string {
 	return e.Content
+}
+
+func (e Entry) getEntryDateAsTime() (time.Time, error) {
+	return time.Parse("2006-01-02 15:04:05", e.Date)
+}
+
+func (e Entry) getRelativeDateString() string {
+	date, err := e.getEntryDateAsTime()
+	if err != nil {
+		return "?"
+	}
+	now := time.Now()
+	diff := now.Sub(date)
+	days := int(diff.Hours() / 24)
+	return fmt.Sprintf("%dd ago", days)
 }
